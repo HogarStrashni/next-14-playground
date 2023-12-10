bfd2d76086655f6b331115d9b3ebbce84b4bb505
Đorđe Matić <djmatic@agfbl.org>
2023-12-10 12:01:40 +0100

    Search by username: - add useDevounce, - useSearchParamState hook

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,15 +1,33 @@
 import { FC } from 'react';
-import { getData } from '@/utils';
-
-import PostCard from '@/components/post-card/post-card';
 import { notFound } from 'next/navigation';
 
-const PostsPage: FC = async () => {
+import { getData } from '@/utils';
+import PostCard from '@/components/post-card/post-card';
+import styles from './page.module.css';
+
+type PostsPageProps = {
+	searchParams: {
+		query: string;
+	};
+};
+
+const PostsPage: FC<PostsPageProps> = async ({ searchParams }) => {	
 	const allPostsAndComments = await getData();
-
+	
 	if (!allPostsAndComments) return notFound();
+	
+	const searchTerm = searchParams?.query;
+	const filteredPosts = searchTerm
+		? allPostsAndComments.filter(
+				(item) =>
+					item.userName?.toLowerCase().includes(searchTerm.toLowerCase())
+			)
+		: allPostsAndComments;
 
-	return allPostsAndComments.map((item) => (
+	if (!filteredPosts.length)
+		return <div className={styles.wrapper}>No posts found!</div>;
+
+	return filteredPosts.map((item) => (
 		<PostCard key={item.id} postData={item} />
 	));
 };
--- src/components/search-field/search-field.tsx
+++ src/components/search-field/search-field.tsx
@@ -1,13 +1,26 @@
 'use client';
 
-import { useState } from 'react';
 import styles from './search-field.module.css';
+import { useDebouncedCallback } from 'use-debounce';
+import useSearchParamState from '@/hooks/use-search-param-state';
 
 const SearchInput = () => {
-	const [query, setQuery] = useState('');
+	const [query, setQuery] = useSearchParamState('query');
+
+	const handleSearch = useDebouncedCallback((searchTerm: string) => {
+		setQuery(searchTerm ? searchTerm : null);
+	}, 400);
+
+	console.log(query);
+
 	return (
 		<div className={styles.wrapper_div}>
-			<input type="search" className={styles.input_field} />
+			<input
+				type="search"
+				className={styles.input_field}
+				defaultValue={query?.toString()}
+				onChange={(e) => handleSearch(e.target.value)}
+			/>
 		</div>
 	);
 };
